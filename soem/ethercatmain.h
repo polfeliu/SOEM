0fe2f33b8777acd90bbbd7b08a4e25b55a58eec6
Benjamin Partzsch <benjamin_partzsch@web.de>
2018-01-28 17:30:21 +0100

    introduce new parameter for PO2SOconfig callback

--- soem/ethercatmain.h
+++ soem/ethercatmain.h
@@ -226,7 +226,9 @@ typedef struct ec_slave
    /** Boolean for tracking whether the slave is (not) responding, not used/set by the SOEM library */
    boolean          islost;
    /** registered configuration function PO->SO */
-   int              (*PO2SOconfig)(uint16 slave);
+   int              (*PO2SOconfig)(uint16 slave, void* user);
+   /** user data for PO2SOconfig */
+   void*            user;
    /** readable name */
    char             name[EC_MAXNAME + 1];
 } ec_slavet;
